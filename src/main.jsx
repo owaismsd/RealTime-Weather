1e52d36f6e2ccc186d38d60037921e7e279cbb62
Owais <owaisahmad.maseed@gmail.com>
2025-08-26 14:48:50 +0500

    Second Commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/RealTime-Weather">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
